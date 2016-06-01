e44fb78644d8c275aeaf9add985ab5033961a420
JayK-13 <210722@hkis.edu.hk>
2016-06-01 09:17:43 +0800

    added arrow key function

--- gamedev.js
+++ gamedev.js
@@ -1,13 +1,39 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
-window.onkeydown = function (e) { if (!e) { e = window.event; }
+  $(document).keydown(function(e) {
+    $(myCar).keydown;
+    switch (e.which) {
 
-var code = e.keyCode; 
-var player = document.getElementById("player");
-var top = parseInt (player.style.top, 10); var left = parseInt (player.style.left, 10); 
-if ( code == 37 ) {  if ( left > 0 ) { player.style.left = left - 10 + 'px'; } } else if ( code == 38 ) {if ( top > 0 ) { player.style.top = top - 10 + 'px'; } } else if ( code == 39 ) { if ( left+player.width+10 < window.innerWidth ) { player.style.left = left + 10 + 'px'; } } else if ( code == 40 ) { if ( top+player.height+10 < window.innerHeight ) { player.style.top = top + 10 +'px'; } } };
-});
+      //move left
+      case 37:
+        $(myCar).animate({
+          left: "-=40px"
+        }, 'fast');
+        break;
+
+        //move up
+      case 38:
+        $(myCar).animate({
+          top: "-=40px"
+        }, 'fast');
+        break;
+
+        //move right
+      case 39:
+        $(myCar).animate({
+          left: "+=40px"
+        }, 'fast');
+        break;
+
+        //move down
+      case 40:
+        $(myCar).animate({
+          top: "+=40px"
+        }, 'fast');
+        break;
+    };
+  }); 
 
 function collision($div1, $div2) {
     var x1 = $div1.offset().left;

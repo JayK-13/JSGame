4273428b9d136612b324ce0131a8266b923ad871
JayK-13 <210722@hkis.edu.hk>
2016-06-03 16:30:12 +0800

    fixing collision if & eslse

--- gamedev.js
+++ gamedev.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     //Your jquery goes here
     var player = $("#player");
         var xl = $("#xl");
-            var citty = $("#citty");
+        var citty = $("#citty");
+$("#textinput").hide();
+  $("#textfield").hide();
+  $("#enter").hide();
 
 
     $(document).keydown(function(e) {
@@ -36,61 +39,9 @@ $(document).ready(function() {
         }
     });
 
-    function collision1($div1, $div2) {
-        var x1 = $div1.offset().left;
-        var y1 = $div1.offset().top;
-        var h1 = $div1.outerHeight(true);
-        var w1 = $div1.outerWidth(true);
-        var b1 = y1 + h1;
-        var r1 = x1 + w1;
-        var x2 = $div2.offset().left;
-        var y2 = $div2.offset().top;
-        var h2 = $div2.outerHeight(true);
-        var w2 = $div2.outerWidth(true);
-        var b2 = y2 + h2;
-        var r2 = x2 + w2;
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-        return true;
-    }
 
-    $.each($('#citty'), function() {
-        if (collision1($('#player'), $(this))) {
-            var cittyquestion = prompt(
-                "I'm tall when I'm young and short when I'm old. What am I?"
-            ).toLowerCase();
-            if (cittyquestion === "candle") {
-                alert("Pass. And don't come back!");
-            } else {
-                alert(
-                    "Nope! Try again, or go away and don't bother me…"
-                );
-            }
-        } else {}
-    });
 
-    function collision2($div1, $div2) {
-        var x1 = $div1.offset().left;
-        var y1 = $div1.offset().top;
-        var h1 = $div1.outerHeight(true);
-        var w1 = $div1.outerWidth(true);
-        var b1 = y1 + h1;
-        var r1 = x1 + w1;
-        var x2 = $div2.offset().left;
-        var y2 = $div2.offset().top;
-        var h2 = $div2.outerHeight(true);
-        var w2 = $div2.outerWidth(true);
-        var b2 = y2 + h2;
-        var r2 = x2 + w2;
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-        return true;
-    $.each($('#xl'), function() {
-            if (collision2($('#player'), $(this))) {
-                $("#alwaysPizza").show().delay(200).fadeOut();
-            }
-        })
-    }
-
-      function collision($div1, $div2) {
+function collision($div1, $div2) {
     var x1 = $div1.offset().left;
     var y1 = $div1.offset().top;
     var h1 = $div1.outerHeight(true);
@@ -107,10 +58,41 @@ $(document).ready(function() {
     if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
     return true;
   }
-$.each($('.otherCar'), function() {
-      if (collision($('#myCar'), $(this))) {
-                    $("#alwaysPizza").show().delay(200).fadeOut();
-                }else{}
-            });
-    
+
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('#citty'), function() {
+      if (collision($('#player'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+    $("#textfield").show();
+        $("#textinput").show();
+        $("#enter").show();
+      }
+        });
+
+
+$("#enter").click(function(){
+
+    var q1 = document.getElementById("textfield").value.toLowerCase();
+
+   //This is what happens if you answer correctly 
+  if (q1 === "candle") {
+      alert("Correct!");
+          }
+        
+  
+  else
+    {
+    }
+
+      }
+    );
+  }, 200); //this is how often it checks for a collision
+
+
+
+
+
 });
+

ace157fd7ee5242cdcf952f1316ad9596ec498b5
JayK-13 <210722@hkis.edu.hk>
2016-06-08 23:54:44 +0800

    final edits

--- gamedev.js
+++ gamedev.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
         var xl = $("#xl");
         var citty = $("#citty");
 $("#textinput").hide();
+$("#textinput1").hide();
   $("#textfield").hide();
-  $("#enter").hide();
+  $(".enter").hide();
+  $("#correct").hide();
+  $("#textinput2").hide();
 
 
     $(document).keydown(function(e) {
@@ -78,21 +81,120 @@ $("#enter").click(function(){
 
    //This is what happens if you answer correctly 
   if (q1 === "candle") {
-      alert("Correct!");
-          }
-        
-  
+    $("#correct").show().delay(5000).fadeOut();
+    $("#textinput").fadeOut();
+$(".enter").fadeOut();
+    }
+
   else
     {
     }
 
       }
     );
-  }, 200); //this is how often it checks for a collision
+  }, 1000); //this is how often it checks for a collision
+
+function collision1($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
+
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('#xl'), function() {
+      if (collision1($('#player'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+    $("#textfield").show();
+        $("#textinput1").show();
+        $("#enter").show();
+      }
+        });
 
 
+$("#enter").click(function(){
+
+    var q2 = document.getElementById("textfield").value.toLowerCase();
+
+   //This is what happens if you answer correctly 
+   if (q2 === "short") {
+    $("#correct").show().delay(5000).fadeOut();
+        $("#textinput1").fadeOut();
+    $("#textfield").fadeOut();
+    $(".enter").fadeOut();
+
+    }
+  else
+    {
+    }
+
+      }
+    );
+  }, 1000); 
+
+function collision2($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+    //below is an if statement - if the variables calculate to the right formula, it will return true or false
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+  }
+
+  window.setInterval(function() {
+    //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
+    $('#result').text('False');
+    //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
+    $.each($('#dlanod'), function() {
+      if (collision1($('#player'), $(this))) { //another if statement. If #myCar DOES hit something, the following will happen:
+    $("#textfield").show();
+        $("#textinput2").show();
+        $("#enter").show();
+      }
+        });
 
 
+$("#enter").click(function(){
+
+    var q3 = document.getElementById("textfield").value.toLowerCase();
+
+   //This is what happens if you answer correctly 
+ if (q3 === "smiles") {
+    $("#correct").show().delay(5000).fadeOut();
+       $("#textinput2").fadeOut();
+    $("#textfield").fadeOut();
+    $(".enter").fadeOut();
+
+    }
+  else
+    {
+    }
+
+      }
+    );
+  }, 1000); 
 
 });
 
